ca722be2268174230c9698e21250933b9b19f363
Yaphet Abraha <yaphet.abraha@gmail.com>
2018-11-23 11:36:49 -0600

    changed gitignore and fixed modal styling

--- src/components/MemeModal/index.js
+++ src/components/MemeModal/index.js
@@ -11,25 +11,29 @@ class MemeModal extends Component {
         open={this.props.openModal}
         onClose={() => this.props.closeEvent()}
         center
+        classNames={{
+          modal: "meme-modal",
+          closeButton: "modal-button-close"
+        }}
       >
         <div className="modal-text-container">
-          <h2 className="modal-text">You are about to create a meme!</h2>
+          <div className="modal-title">Create Meme</div>
 
-          <h2 className="modal-text">Are you finished editing?</h2>
+          <h2 className="modal-subtitle">Are you finished editing?</h2>
         </div>
 
         <div className="modal-buttons">
-          <button
-            className="modal-button-confirm"
-            onClick={() => this.props.confirmEvent()}
-          >
-            Yes
-          </button>
           <button
             className="modal-button-cancel"
             onClick={() => this.props.closeEvent()}
           >
             No
+          </button>
+          <button
+            className="modal-button-confirm"
+            onClick={() => this.props.confirmEvent()}
+          >
+            Yes
           </button>
         </div>
       </Modal>
